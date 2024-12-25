5fdcdc03a3c74ef61f70d60f3e8c48ccccc5d039
Usman Ejaz <mohd.usman2000@gmail.com>
2024-12-26 01:00:31 +0500

    4 modules done

--- CodeEditor/src/Components/App.jsx
+++ CodeEditor/src/Components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [html, setHtml] = useLocalStorage('html', '');
   const [css, setCss] = useLocalStorage('css', '');
   const [js, setJs] = useLocalStorage('js', '');
-  const [screenSize, setScreenSize] = useState();
+  const [screenSize, setScreenSize] = useState(1686);
 
   const srcDoc = `
     <html>
@@ -20,46 +20,59 @@ function App() {
   return (
     <>
       <div className="pane top-pane">
-        <TextEditor 
-          language="xml" 
-          title="HTML" 
-          value={html} 
-          onChange={setHtml} 
-        />  
-        <TextEditor 
-          language="css" 
-          title="CSS" 
-          value={css} 
-          onChange={setCss} 
-        />  
-        <TextEditor 
-          language="javascript" 
-          title="JS" 
-          value={js} 
-          onChange={setJs} 
+        <TextEditor
+          language="xml"
+          title="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <TextEditor
+          language="css"
+          title="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <TextEditor
+          language="javascript"
+          title="JS"
+          value={js}
+          onChange={setJs}
         />
       </div>
-      <label htmlFor="screenSize">Screen Size: </label>
-      <select name="screenSize" id="screenSize" 
-        onChange={() => {
-          setScreenSize(value);
-        }}>
-        <option value="">Select Screen</option>
-        <option value="390">Mobile</option>
-        <option value="1024">Tablet</option>
-        <option value="1750">Desktop</option>
-      </select>
-      <div className="Bottom-pane">
-        <iframe
-         srcDoc={srcDoc}
-         title='output'
-         sandbox='allow-scripts'   
-         frameBorder="0"
-         width="100%"
-         height="100%"
-         />
+
+      <div className='settings'>
+        <h2>Settings</h2> <hr />
+        <h3>Screen Size</h3>
+        <label htmlFor="screenSize">Select Screen Size: </label>
+        <select name="screenSize" id="screenSize" width="0.5rem"
+          onChange={(event) => {
+            setScreenSize(Number(event.target.value));
+          }}>
+          <option value="1686">Select Screen</option>
+          <option value="390">Mobile</option>
+          <option value="1024">Tablet</option>
+          <option value="1750">Desktop</option>
+        </select>
+        <br />
+        <label htmlFor="screenSize">Enter Screen Size: </label>
+        <input type="number" placeholder='or enter screen size...' width="0.5rem"
+          onChange={(event) => {
+            setScreenSize(Number(event.target.value));
+          }}
+          />
       </div>
-      
+
+      <div className="Bottom-pane" style={{textAlign: 'center', backgroundColor: 'GrayText'}}>
+        <iframe
+          srcDoc={srcDoc}
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder="0"
+          width={screenSize}
+          height="100%"
+        />
+      </div>
+
     </>
   )
 }
