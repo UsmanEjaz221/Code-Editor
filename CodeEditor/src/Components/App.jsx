b50983adaded6886cf41f3ee251e1da57d3082de
Usman Ejaz <mohd.usman2000@gmail.com>
2024-12-28 22:47:48 +0500

    js functions now working

--- CodeEditor/src/Components/App.jsx
+++ CodeEditor/src/Components/App.jsx
@@ -84,7 +84,7 @@ function App() {
         <iframe
           srcDoc={srcDoc}
           title='output'
-          sandbox='allow-scripts'
+          sandbox='allow-scripts allow-modals'
           width={screenSize}
           height="100%"
         />
